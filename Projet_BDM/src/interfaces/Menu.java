05ae19b1243e1dabefcd46e0925deeca8aa875f2
Zen0b1a <gillet.ann@gmail.com>
2017-04-09 18:56:45 +0200

    Interface commissariat

--- Projet_BDM/src/interfaces/Menu.java
+++ Projet_BDM/src/interfaces/Menu.java
@@ -27,21 +27,26 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        Commissariat = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new java.awt.GridLayout());
+
+        Commissariat.setText("Commissariat");
+        Commissariat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CommissariatActionPerformed(evt);
+            }
+        });
+        getContentPane().add(Commissariat);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void CommissariatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CommissariatActionPerformed
+        DlgCommissariat dlgCommissariat = new DlgCommissariat();
+        dlgCommissariat.setVisible(true);
+    }//GEN-LAST:event_CommissariatActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +84,6 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Commissariat;
     // End of variables declaration//GEN-END:variables
 }
