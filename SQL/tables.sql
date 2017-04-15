019a1312f3af0af8d5b43d44059f8c73d9574d6c
Zen0b1a <gillet.ann@gmail.com>
2017-04-15 19:13:09 +0200

    Début modif SQL + schéma

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -7,38 +7,34 @@ DROP TABLE bdm_preuve CASCADE CONSTRAINTS;
 DROP TABLE bdm_crime CASCADE CONSTRAINTS;
 DROP TABLE bdm_temoignage CASCADE CONSTRAINTS;
 
-CREATE TABLE bdm_commissariat OF bdm_commissariat_type
-(PRIMARY KEY(numeroCo)
-)
-NESTED TABLE enquetesEnCours STORE AS tab_enquetes_en_cours,
-NESTED TABLE enquetesResolues STORE AS tab_enquetes_resolues;
-
 CREATE TABLE bdm_enqueteur OF bdm_enqueteur_type
-(PRIMARY KEY(id),
-commissariatE SCOPE IS bdm_commissariat);
+(PRIMARY KEY(id));
 
 CREATE TABLE bdm_suspect OF bdm_suspect_type
-(PRIMARY KEY(id));
+(PRIMARY KEY(id),
+CHECK(etat IN ('coupable', 'disculpé', 'non défini'));
 
 CREATE TABLE bdm_victime OF bdm_victime_type
-(PRIMARY KEY(id));
+(PRIMARY KEY(id),
+CHECK(etat IN ('vivant', 'mort'));
 
 CREATE TABLE bdm_enquete OF bdm_enquete_type
-(PRIMARY KEY(numeroE))
+(PRIMARY KEY(id),
+CHECK(etat IN ('en-cours', 'résolue'))
 NESTED TABLE enqueteurs STORE AS tab_enqueteurs,
 NESTED TABLE suspects STORE AS tab_suspects,
 NESTED TABLE coupables STORE AS tab_coupables;
 
 CREATE TABLE bdm_preuve OF bdm_preuve_type
-(PRIMARY KEY(numeroP),
+(PRIMARY KEY(id),
 enqueteP SCOPE IS bdm_enquete);
 
 CREATE TABLE bdm_crime OF bdm_crime_type
-(PRIMARY KEY(numeroCr),
+(PRIMARY KEY(id),
 enqueteC SCOPE IS bdm_enquete)
 NESTED TABLE victimes STORE AS tab_victimes;
 
 CREATE TABLE bdm_temoignage OF bdm_temoignage_type
-(PRIMARY KEY(numeroT),
+(PRIMARY KEY(id),
 crimeT SCOPE IS bdm_crime);
 
--- SQL/types.sql
+++ SQL/types.sql
@@ -34,15 +34,12 @@ CREATE TYPE bdm_telephones_type AS VARRAY(2) OF bdm_telephone_type;
 
 CREATE TYPE bdm_personne_type AS OBJECT 
 (id INTEGER, nom VARCHAR2(50), prenom VARCHAR2(50), adresse bdm_adresse_type, telP bdm_telephones_type, 
-photo SI_StillImage)
+photo ORDImage)
 NOT INSTANTIABLE NOT FINAL;
 / 
 
-CREATE TYPE bdm_commissariat_type;
-/
-
 CREATE TYPE bdm_enqueteur_type UNDER bdm_personne_type
-(badge INTEGER, commissariatE REF bdm_commissariat_type);
+(badge INTEGER);
 /
 
 CREATE TYPE bdm_enqueteur_ref_type AS OBJECT (enqueteurR REF bdm_enqueteur_type);
@@ -52,7 +49,7 @@ CREATE TYPE bdm_enqueteurs_type AS TABLE OF bdm_enqueteur_ref_type;
 /
 
 CREATE TYPE bdm_suspect_type UNDER bdm_personne_type
-(alibi VARCHAR2(500), etatS VARCHAR2(50));
+(alibi VARCHAR2(500), etat VARCHAR2(50));
 /
 
 CREATE TYPE bdm_suspect_ref_type AS OBJECT (suspectR REF bdm_suspect_type);
@@ -62,7 +59,7 @@ CREATE TYPE bdm_suspects_type AS TABLE OF bdm_suspect_ref_type;
 /
 
 CREATE TYPE bdm_victime_type UNDER bdm_personne_type
-(etatV VARCHAR2(50));
+(etat VARCHAR2(50));
 /
 
 CREATE TYPE bdm_victime_ref_type AS OBJECT (victimeR REF bdm_victime_type);
@@ -72,7 +69,7 @@ CREATE TYPE bdm_victimes_type AS TABLE OF bdm_victime_ref_type;
 /
 
 CREATE TYPE bdm_enquete_type AS OBJECT 
-(numeroE INTEGER, etatE VARCHAR2(50), enqueteurs bdm_enqueteurs_type, suspects bdm_suspects_type, 
+(id INTEGER, etat VARCHAR2(50), enqueteurs bdm_enqueteurs_type, suspects bdm_suspects_type, 
 coupables bdm_suspects_type);
 /
 
@@ -83,7 +80,7 @@ CREATE TYPE bdm_enquetes_type AS TABLE OF bdm_enquete_ref_type;
 /
 
 CREATE TYPE bdm_preuve_type AS OBJECT 
-(numeroP INTEGER, descr VARCHAR2(500), enqueteP REF bdm_enquete_type)
+(id INTEGER, description VARCHAR2(500), enqueteP REF bdm_enquete_type)
 NOT INSTANTIABLE NOT FINAL;
 / 
 
@@ -100,15 +97,11 @@ CREATE TYPE bdm_preuve_video_type UNDER bdm_preuve_type
 /
 
 CREATE TYPE bdm_crime_type AS OBJECT
-(numeroCr INTEGER, fait VARCHAR2(50), lieu VARCHAR2(50), dateC DATE, enqueteC REF bdm_enquete_type, 
+(id INTEGER, fait VARCHAR2(50), lieu VARCHAR2(50), date DATE, enqueteC REF bdm_enquete_type, 
 victimes bdm_victimes_type);
 /
 
 CREATE TYPE bdm_temoignage_type AS OBJECT 
-(numeroT INTEGER, dateT DATE, contenu VARCHAR2(1000), personneT REF bdm_personne_type, crimeT REF bdm_crime_type);
+(id INTEGER, date DATE, contenu VARCHAR2(1000), personneT REF bdm_personne_type, crimeT REF bdm_crime_type);
 /
 
-CREATE TYPE bdm_commissariat_type AS OBJECT
-(numeroCo INTEGER, adresse bdm_adresse_type, telC bdm_telephone_type, enquetesEnCours bdm_enquetes_type, 
-enquetesResolues bdm_enquetes_type);
-/
